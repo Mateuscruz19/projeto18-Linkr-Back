7a6dbb0128166dd0a44653d8262fe48a016fc0b7
Victor Motta <victor.mottag@gmail.com>
2023-03-08 18:53:23 -0300

    fix: corrigindo bug authcontroller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import { insertNewUser, findExistUser, exportSession } from '../repository/auth.repository.js';
-
+//signup
 export async function signUp(req, res) {
   const { username, email, password, picture } = res.locals.user;
 
