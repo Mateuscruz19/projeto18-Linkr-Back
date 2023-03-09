c34a5cdb6297fc7d6ca8ddb3e4d810fd46d0debe
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-03-09 16:42:45 -0300

    chore: implement hashtags.repository

--- src/repository/hashtags.repository.js
+++ src/repository/hashtags.repository.js
@@ -1,7 +1,21 @@
 import db from "../database/db";
 
 export async function getTrendings(){
-	return await db.query.apply(`
-		SELECT * FROM 
+	return await db.query(`
+		SELECT hashtags.name FROM hashtags
+		GROUP BY hashtags.name
+		ORDER BY COUNT(hashtags.name) DESC
+		LIMIT 10
+		;
+	`);
+}
+
+export async function getByHashtags(hashtag){
+	return await db.query(`
+		SELECT posts.id, posts. user_id, posts.link, posts.description, posts.created_at, posts.updated_at FROM posts
+		JOIN hashtags ON hashtags.post_id = posts.id
+		WHERE hashtags.name = '#hash9'
+		ORDER BY posts.created_at DESC
+		;
 	`);
 }
